af0f8dd0cd40096e9028b357e57b1288662f76a8
Nathan Rajlich <nathan@tootallnate.net>
2014-10-17 12:58:45 -0700

    test: add "dispatchable" test

--- test/test.js
+++ test/test.js
@@ -24,6 +24,38 @@ describe('ProgressEvent', function () {
       assert.equal(p.total, 0);
     });
 
+    it('should create a `ProgressEvent` instance that is dispatchable', function () {
+      var p = new PE('progress', {
+        lengthComputable: true,
+        loaded: 4,
+        total: 10
+      });
+
+      var called = false;
+      var e = document.createElement('div');
+      document.body.appendChild(e);
+
+      function onprogress (ev) {
+        assert.equal(p.type, 'progress');
+        assert.equal(p.lengthComputable, true);
+        assert.equal(p.loaded, 4);
+        assert.equal(p.total, 10);
+        called = true;
+      }
+
+      assert(!called);
+      if (e.dispatchEvent) {
+        e.addEventListener('progress', onprogress, false);
+        e.dispatchEvent(p);
+      } else {
+        // IE <= 8 will only allow us to fire "known" event names,
+        // so we need to fire "click" instead of "progress :\
+        e.attachEvent('onclick', onprogress);
+        e.fireEvent('onclick', p);
+      }
+      assert(called);
+    });
+
   });
 
 });
